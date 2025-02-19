087b7db51ea75103be36bee95a99a1d19ee840d0
Muxsimov <muxsimovasadbek665@gmail.com>
2025-02-19 18:49:31 +0500

    finished

--- src/assets/components/PaginationButtons.jsx
+++ src/assets/components/PaginationButtons.jsx
@@ -83,6 +83,7 @@ function CatalogPage() {
               onClick={() => setPagination(page)}
               key={page}
             >
+              
               {page}
             </Button>
           );
